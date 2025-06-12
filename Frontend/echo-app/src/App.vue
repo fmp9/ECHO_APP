f01a444f0f0b15985e08e3930d98c5e2c2913022
fmp9 <flo99p@gmx.at>
2025-06-12 09:24:55 +0200

    v02

--- Frontend/echo-app/src/App.vue
+++ Frontend/echo-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-view></router-view>
+  <router-view></router-view>
   </div>
 </template>
 
@@ -8,21 +8,6 @@
 
 </script>
 
-<style lang="scss">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-html, body{
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-  background-color: var(--main-col);
-  color: var(--text-col);
-  font-family: 'Poppins', sans-serif;
-  font-size: 18px;
-  padding: var(--margin-sm-md) var(--margin-lg);
-}
+<style lang="scss" scoped>
 
 </style>
